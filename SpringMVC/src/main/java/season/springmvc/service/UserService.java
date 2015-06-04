f269f14a6b04db1a58e0cef2f4b033a875c2255c
fishblue <boycechou@gmail.com>
2015-06-04 17:49:37 +0800

    基于maven的springmvc框架构建，包括组件： 1。mybatis持久层 2.alibaba druid数据库连接池 3.log4j日志
    Signed-off-by: fishblue <boycechou@gmail.com>

---
+++ SpringMVC/src/main/java/season/springmvc/service/UserService.java
@@ -0,0 +1,11 @@
+package season.springmvc.service;
+
+import season.springmvc.model.User;
+/**
+ * ÒµÎñÂß¼­²ã
+ * @author Administrator
+ *
+ */
+public interface UserService {
+	public int insertUser(User user);
+}
